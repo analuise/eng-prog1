949e1a14156478b96c7b0d191cae153cb471a132
analuise <analuisedeabreu@gmail.com>
2024-04-26 16:22:09 -0300

    Atualizações no Código

--- Aula 02 - switch-case/main.c
+++ Aula 02 - switch-case/main.c
@@ -4,12 +4,23 @@
 int main() {
     int dado;
 
-    printf("Entre com um valor inteiro: ");
+    printf("Escolha um atendente 0-9: ");
+    printf("\n0 - Falar com Natalia");
+    printf("\n1 - Falar com Miriã");
+    printf("\n2 - Falar com ");
+    printf("\n3 - Falar com ");
+    printf("\n4 - Falar com ");
+    printf("\n5 - Falar com ");
+    printf("\n6 - Falar com ");
+    printf("\n7 - Falar com ");
+    printf("\n8 - Falar com ");
+    printf("\n9 - Retornar ao menu inicial ");
+
     scanf("%d", &dado);
 
     switch(dado)
     {
-        case 1: 
+        case 0: 
     }
 
 
