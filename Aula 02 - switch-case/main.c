29bff8fd290ac6e6f2c7b82340fdb67775f7b776
analuise <analuisedeabreu@gmail.com>
2024-04-26 16:16:53 -0300

    Iniciando Código

---
+++ Aula 02 - switch-case/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int main() {
+    int dado;
+
+    printf("Entre com um valor inteiro: ");
+    scanf("%d", &dado);
+
+    switch(dado)
+    {
+        case 1: 
+    }
+
+
+
+
+}
+
+
+
